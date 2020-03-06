e9f94c958a63548feb15b5036b1beacd345a7f9a
ChrisThor <krister.wolfhard@it-designers.de>
2020-03-06 12:28:51 +0100

    Change menu navigation

--- scenes/MainMenu/StartPlanet.gd
+++ scenes/MainMenu/StartPlanet.gd
@@ -9,10 +9,10 @@ export var button_blocked = .2
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	pass # Replace with function body.
 
 func select_button():
-	print("Exit Game")
+	print("Start Planet")
 	self.button_selected = true
 	self.button_blocked = .2
 	self.grab_focus()
@@ -22,13 +22,9 @@ func _process(delta):
 	if self.button_selected and self.button_blocked <= 0:
 		if Input.is_action_pressed("ui_down"):
 			self.button_selected = false
-			get_parent().get_node("StartSpace").select_button()
+			get_parent().get_node("ExitGame").select_button()
 		elif Input.is_action_pressed("ui_up"):
 			self.button_selected = false
-			get_parent().get_node("StartPlanet").select_button()
+			get_parent().get_node("StartSpace").select_button()
 	elif self.button_blocked > 0:
 		self.button_blocked -= delta
-
-
-func _on_ExitGame_pressed():
-	get_tree().quit(0)
--- scenes/MainMenu/StartSpace.gd
+++ scenes/MainMenu/StartSpace.gd
@@ -4,18 +4,31 @@ extends Button
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
+export var button_selected = true
+export var button_blocked = .2
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	self.select_button()
+
+func select_button():
+	print("Start Space")
+	self.button_selected = true
+	self.button_blocked = .2
 	self.grab_focus()
 
-#func pressed():
-	#get_tree().change_scene("res://scenes/Space/Space.tscn")
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	if self.button_selected and self.button_blocked <= 0:
+		if Input.is_action_pressed("ui_down"):
+			self.button_selected = false
+			get_parent().get_node("StartPlanet").select_button()
+		elif Input.is_action_pressed("ui_up"):
+			self.button_selected = false
+			get_parent().get_node("ExitGame").select_button()
+	elif self.button_blocked >= 0:
+		self.button_blocked -= delta
+		print(self.button_blocked)
 
 
 func _on_StartSpace_pressed():
