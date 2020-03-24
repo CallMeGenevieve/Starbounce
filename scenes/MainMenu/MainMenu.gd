2b4eef5a51bf793ba059e66c2e1eeab091d48418
ChrisThor <krister.wolfhard@it-designers.de>
2020-03-24 10:08:11 +0100

    Update Main Menu

---
+++ scenes/MainMenu/MainMenu.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+var window_size = Vector2()
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	self.window_size = Vector2(ProjectSettings.get_setting("display/window/size/width"), ProjectSettings.get_setting("display/window/size/width"))
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
