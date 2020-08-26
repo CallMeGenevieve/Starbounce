f3e366757b9170a37b9b2553561481a1bfc68e3f
CallMeGenevieve <wejink441@gmail.com>
2020-08-26 13:53:32 +0200

    Add steering particles

--- scenes/HopeShip/HopeShip.gd
+++ scenes/HopeShip/HopeShip.gd
@@ -18,6 +18,8 @@ var index = 0
 var ui_node
 var positional_particles
 var crashed = false
+var dir_x
+var dir_y
 
 
 func _ready():
@@ -30,11 +32,13 @@ func apply_acceleration(delta):
 	#get_node("Positional Particles").rotation += self.angle_speed * delta
 	get_node("CollisionShape2D").rotation += self.angle_speed * delta
 	
+	dir_x = sin(get_node("Sprite").rotation)
+	dir_y = -cos(get_node("Sprite").rotation)
+	
 	if self.direction != 0:
 		if not $"Thruster Particles".emitting:
 			$"Thruster Particles".emitting = true
-		var dir_x = sin(get_node("Sprite").rotation)
-		var dir_y = -cos(get_node("Sprite").rotation)
+
 		self.thrust.x = dir_x * self.thrust_power
 		self.thrust.y = dir_y * self.thrust_power
 		
@@ -56,6 +60,17 @@ func apply_speed(delta):
 func apply_tick(delta):
 	apply_acceleration(delta)
 	apply_speed(delta)
+	
+	if $"Steering Particles Left".emitting:
+		$"Steering Particles Left".position.x = sin(get_node("Sprite").rotation) * 8
+		$"Steering Particles Left".position.y = -cos(get_node("Sprite").rotation) * 8
+		$"Steering Particles Left".process_material.direction.x = cos(get_node("Sprite").rotation)
+		$"Steering Particles Left".process_material.direction.y = sin(get_node("Sprite").rotation)
+	if $"Steering Particles Right".emitting:
+		$"Steering Particles Right".position.x = sin(get_node("Sprite").rotation) * 8
+		$"Steering Particles Right".position.y = -cos(get_node("Sprite").rotation) * 8
+		$"Steering Particles Right".process_material.direction.x = -cos(get_node("Sprite").rotation)
+		$"Steering Particles Right".process_material.direction.y = -sin(get_node("Sprite").rotation)
 	
 	# I know this code is duplicate but idk what a better solution would be.
 	# Is there a possibility of importing functions here?!
@@ -84,11 +99,17 @@ func _process(delta):
 				self.angle_speed += delta * 2
 			elif self.angle_speed != 6:
 				self.angle_speed = 6
+			$"Steering Particles Right".emitting = true
+		elif Input.is_action_just_released("turn_ship_right"):
+			$"Steering Particles Right".emitting = false
 		if Input.is_action_pressed("turn_ship_left"):
 			if self.angle_speed > -6:
 				self.angle_speed -= delta * 2
 			elif self.angle_speed != -6:
 				self.angle_speed = -6
+			$"Steering Particles Left".emitting = true
+		elif Input.is_action_just_released("turn_ship_left"):
+			$"Steering Particles Left".emitting = false
 
 
 func _on_HopeShip_area_entered(area):
